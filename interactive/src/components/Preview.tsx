e2b017b302abb1e0ceab75fcf050d1ebe6b98810
Kurogoma <gogottt2009-hp@yahoo.co.jp>
2024-01-08 16:30:27 +0900

    Specify svg max size by CSS

--- interactive/src/components/Preview.tsx
+++ interactive/src/components/Preview.tsx
@@ -15,15 +15,11 @@ export const Preview: FC<PreviewProps> = ({
   width,
   height,
 }: PreviewProps) => {
-  const maxWidth = document.body.clientWidth - 100;
-  const maxHeight = maxWidth * 0.25;
   return (
     <div className={style.chararea}>
       <svg
         viewBox={`0 0 ${width} ${height}`}
         preserveAspectRatio="xMinYMid meet"
-        width={maxWidth}
-        height={maxHeight}
       >
         {glyphs.map(({ dList, offsetX }, i) => (
           <g key={i} transform={`translate(${offsetX},0)`}>
