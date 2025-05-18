f4c25340a1ad81f061144c6600e255f08aede359
Kurogoma <gogottt2009-hp@yahoo.co.jp>
2025-05-18 15:50:17 +0900

    Use named imports

--- interactive/src/main.tsx
+++ interactive/src/main.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
 import { App } from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById("react_root")!).render(
-  <React.StrictMode>
+createRoot(document.getElementById("react_root")!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
