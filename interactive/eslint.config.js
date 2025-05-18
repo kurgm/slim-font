816483d7da1e85731e6efef1c6186d9b81731a55
Kurogoma <gogottt2009-hp@yahoo.co.jp>
2025-05-18 16:09:26 +0900

    Migrate to ESLint flat config

---
+++ interactive/eslint.config.js
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      reactHooks.configs["recommended-latest"],
+    ],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  }
+);
---
+++ slim/eslint.config.js
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+    ],
+    files: ["**/*.ts"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.es2020,
+    },
+  }
+);
